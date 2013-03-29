1d02c8840125c23a7cda3379c5d3c12e650d8e4b
aina rubies <aina.rubies@isoccer.org>
2013-03-29 09:43:16 -0700

    testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AinaApp::Application.config.secret_token = 'b83065563d010f95a9683c996e5a439de84043201cde5d22ccdb7365b27e1b9835f7417674533f9bb5d7324ecd9f3ed1ba01d7d054289b81882b22e3c6f51025'
